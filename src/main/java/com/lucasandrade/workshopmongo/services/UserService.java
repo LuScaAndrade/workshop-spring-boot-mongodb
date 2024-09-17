123b6eafe58d8be9720ff8ec3dcae17f846db7e1
Lucas Andrade <Lucasavitorino1@gmail.com>
2024-09-17 18:32:09 -0300

    Insercao de usuario com POST

--- src/main/java/com/lucasandrade/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasandrade/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasandrade.workshopmongo.domain.User;
+import com.lucasandrade.workshopmongo.dto.UserDTO;
 import com.lucasandrade.workshopmongo.repository.UserRepository;
 import com.lucasandrade.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 	    Optional<User> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
